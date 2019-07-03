9a6e7bab2be69c30de40d163892e7dee493acdd1
mapleclover7 <mapleclove@gmail.com>
2019-07-03 10:24:05 +0900

    Basic0703

---
+++ Agharta2D/Assets/01.Scripts/MainOption.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+//유은호
+public class MainOption : MonoBehaviour
+{
+    private int select = 0;
+    public GameObject[] menu;
+
+    void Start()
+    {
+        coloring();//시작하면 바로 선택된 메뉴(0) 색칠
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))//엔터를 쳤을시
+        {
+            switch (select)
+            {
+                case 0:
+                    SceneManager.LoadScene("Game");//게임시작Scene
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Option");//게임옵션Scene
+                    break;
+                case 2:
+                    Quit();
+                    //게임종료
+                    break;
+            }
+        }
+        else if(Input.anyKeyDown)
+        {
+            MenuSelect();
+            coloring();
+        }
+    }
+
+    void MenuSelect()//메뉴선택
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))//메뉴선택 위 방향키 
+        {
+            select -= 1;
+            if (select == -1)//맨위에서 한번더 위로 눌렀을시
+            {
+                select = 2;//맨아래 선택지로 내려감
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))//메뉴선택 아래 방향키 
+        {
+            select += 1;
+            if (select == 3)//맨위에서 한번더 아래 눌렀을시
+            {
+                select = 0;//맨위 선택지로 올라감
+            }
+        }
+    }
+
+    void coloring()//선택되있는 메뉴 표시
+    {
+        for(int i=0; i<menu.Length; i++)//선택되있는 메뉴확인 및 텍스트 색 변경 ※추후 메뉴옆 화살표 띄우는것으로 변경 예정
+        {
+            if(select == i)
+            {
+                menu[i].GetComponent<TextMeshProUGUI>().color = Color.red;
+            }
+            else
+            {
+                menu[i].GetComponent<TextMeshProUGUI>().color = Color.white;
+            }
+        }
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else 
+        Application.Quit();
+#endif
+    }
+}
---
+++ Agharta2D/Assets/01.Scripts/PlayerControl.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//유은호
+public class PlayerControl : MonoBehaviour
+{
+    private float horizontal; //움직임 감지 변수
+    public float speed, jump; //횡움직임 속도 점프 속도 변수
+    public Rigidbody2D Rigidbody;
+
+    private bool canJump = false;
+    private bool facingRight = true;
+    public int HP = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HP > 0)//캐릭터가 사망하지 않았다면(HP 0이상이라면)
+        {
+            if (facingRight && Input.GetAxisRaw("Horizontal") == -1)
+            {
+                flip();//좌우 반전
+            }
+            else if (!facingRight && Input.GetAxisRaw("Horizontal") == 1)
+            {
+                flip();
+            }
+
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                PCMove();//좌우 움직임
+            }
+            
+            if (canJump && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))//점프가능한지? + 점프키 확인
+            {
+                canJump = false;
+                Rigidbody.AddForce(Vector2.up * jump, ForceMode2D.Impulse);//점프 움직임
+            }
+        }
+        else
+        {
+            PCDied();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")//점프 1회 한정
+        {
+            canJump = true;
+        }
+        if(collision.gameObject.tag == "Enemy")//몬스터 피격시 HP 1감소
+        {
+            HP -= 1;
+        }
+    }
+
+    void PCMove()//좌우 움직임
+    {
+        horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        this.transform.Translate(horizontal, 0.0f, 0.0f);
+    }
+
+    void PCDied()//플레이어 사망시
+    {
+
+    }
+
+    void PCAttack()//플레이어 공격시
+    {
+
+    }
+
+    void PCSkill()//플레이어 스킬변경
+    {
+
+    }
+
+    
+}
